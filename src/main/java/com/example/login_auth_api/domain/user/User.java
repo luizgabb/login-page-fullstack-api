3d129113c85bc34890d6f880b970a0aad144f3ef
luiz.stedile <luiz.stedile@seinfra.ce.gov.br>
2025-10-13 16:54:14 -0300

    Initial commit

---
+++ src/main/java/com/example/login_auth_api/domain/user/User.java
@@ -0,0 +1,30 @@
+package com.example.login_auth_api.domain.user;
+
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String name;
+    private String email;
+    private String password;
+
+}
